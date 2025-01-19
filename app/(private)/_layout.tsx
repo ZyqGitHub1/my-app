5ad1aa591b6d8b00a97eb501303071589b0ebc55
zyq <qqdhsy@126.com>
2025-01-19 22:21:37 +0800

    first commit

---
+++ app/(private)/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot, Stack } from "expo-router";
+
+export default function Index() {
+  console.log("(private)/_layout.tsx");
+  return <Stack/>
+}
---
+++ app/(private)/item.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Item() {
+  return (
+    <View>
+      <Text>商品详情页</Text>
+    </View>
+  );
+}
